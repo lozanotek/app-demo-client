3433a6c3809d18410839e5ec7f2d49301e641dc0
Javier Lozano <javier@lozanotek.com>
2016-09-09 21:15:51 -0500

    Initial source addition

---
+++ src/AppDemoClient/ValueClient.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace AppDemo.Client
+{
+    public class ValueClient
+    {
+        public IList<string> GetValues()
+        {
+            return new List<string>();
+        } 
+    }
+}
