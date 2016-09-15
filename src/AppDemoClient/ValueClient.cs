0c7e73ee8d9563e200ef4367a068f11e028cc197
Javier Lozano <javier@lozanotek.com>
2016-09-15 09:59:36 -0500

    Implemented the client

--- src/AppDemoClient/ValueClient.cs
+++ src/AppDemoClient/ValueClient.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace AppDemo.Client
 {
@@ -6,7 +11,36 @@ namespace AppDemo.Client
     {
         public IList<string> GetValues()
         {
-            return new List<string>();
-        } 
+            return GetValuesAsync().Result;
+        }
+
+        public async Task<IList<string>> GetValuesAsync()
+        {
+            var client = GetHttpClient();
+            var response = await client.GetAsync("api/values");
+            if (!response.IsSuccessStatusCode)
+            {
+                return new List<string>();
+            }
+
+            return await response.Content.ReadAsAsync<List<string>>();
+        }
+
+        protected virtual HttpClient GetHttpClient()
+        {
+            var apiUrl = GetApiUrl();
+            var client = new HttpClient {BaseAddress = new Uri(apiUrl)};
+
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            return client;
+        }
+
+        protected virtual string GetApiUrl()
+        {
+            var demoUrl = ConfigurationManager.AppSettings["AppDemo.Url"];
+            return demoUrl;
+        }
     }
 }
